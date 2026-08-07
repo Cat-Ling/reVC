e97c8c6fdc842089fb3291b63d08415e4071b874
Violet Developer <dev@violet.local>
2026-08-07 13:12:46 +0530

    Snapshot: Fixed CdStreamInit Linux assertion crash

--- src/core/CdStream_posix.cpp
+++ src/core/CdStream_posix.cpp
@@ -60,7 +60,7 @@ re3_sem_close(sem_t* sem)
 	free(sem);
 }
 
-#elifndef ANDROID
+#elif !defined(ANDROID)
 
 #define RE3_SEM_OPEN re3_sem_open
 sem_t*
@@ -238,11 +238,11 @@ CdStreamInit(int32 numChannels)
     debug("%s\n", imgPath);
 
     if((statvfs(imgPath, &fsInfo)) < 0)
-#elifndef ANDROID
+#elif !defined(ANDROID)
     if((statvfs("models/gta3.img", &fsInfo)) < 0)
 #endif
 	{
-		CDTRACE("can't get filesystem info");
+		CDTRACE("can't get filesystem info: errno %d (%s)", errno, strerror(errno));
 		ASSERT(0);
 		return;
 	}
